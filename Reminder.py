bd8134dfc21ca15c9b5de6bd94ba19648c0be070
Alexanc382 <alexanc382@gmail.com>
2025-07-23 14:29:57 +0300

    added time setting function

--- Reminder.py
+++ Reminder.py
@@ -1,15 +1,32 @@
 from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import simpledialog as sd
-from datetime import datetime
+import datetime
 import os
 import time
 import pygame
 
+def set():
+    rem = sd.askstring('Reminder time', 'Enter the reminder-time in 24-hour format(hh:mm)')
+    if rem:
+        try:
+            hour_user = int(rem.split(':')[0])
+            minute_user = int(rem.split(':')[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour_user,minute=minute_user)
+            print(dt)
+            t = dt.fromtimestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror('Error', f'Error "{e}" occurred' )
+
 window = Tk()
 window.title('Reminder')
-label = Label(text='Please set a reminder')
+label = Label(text='Please set a reminder:', width=50, height=30)
 label.pack(pady=10)
-set_button = Button('set the reminder', command=set)
+set_button = Button(text='set the reminder', command=set)
 set_button.pack()
+
+window.mainloop()
 
